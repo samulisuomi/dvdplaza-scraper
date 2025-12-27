569d57b619e6e488a9b9101058828c77408d36a0
Samuli Suomi <3490745+samulisuomi@users.noreply.github.com>
2025-12-27 14:07:40 +0200

    Rewrite with fetch and async

--- get-search-url-initial.js
+++ get-search-url-initial.js
@@ -1,60 +1,39 @@
-const https = require("https");
-const querystring = require("querystring");
-
-function getSearchUrl(userName) {
-  return new Promise((resolve, reject) => {
-    const data = querystring.stringify({
-      users: userName,
-    });
-
-    const options = {
-      hostname: "www.dvdplaza.fi",
-      path: "/search/search",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Connection: "close",
-      },
-    };
-
-    const req = https.request(options, (res) => {
-      // Node.js normalizes headers to lowercase
-      const location = res.headers.location;
-
-      // Consume the response to allow the connection to close
-      res.on("data", () => {});
-      res.on("end", () => {});
-
-      if (location) {
-        resolve(location);
-        // Destroy the connection to ensure immediate exit
-        res.destroy();
-      } else {
-        reject(new Error("No Location header found in response"));
-      }
-    });
-
-    req.on("error", (error) => {
-      reject(error);
-    });
-
-    req.write(data);
-    req.end();
+async function getSearchUrl(userName) {
+  const data = new URLSearchParams({
+    users: userName,
   });
+
+  const response = await fetch("https://www.dvdplaza.fi/search/search", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: data.toString(),
+    redirect: "manual",
+  });
+
+  const location = response.headers.get("location");
+
+  if (location) {
+    return location;
+  } else {
+    throw new Error("No Location header found in response");
+  }
 }
 
 // If run directly, execute with parameters from CLI
 if (require.main === module) {
   const userName = process.argv[2];
-  getSearchUrl(userName)
-    .then((location) => {
+  (async () => {
+    try {
+      const location = await getSearchUrl(userName);
       console.log(location);
       process.exit(0);
-    })
-    .catch((error) => {
+    } catch (error) {
       console.error("Error:", error.message);
       process.exit(1);
-    });
+    }
+  })();
 }
 
 module.exports = getSearchUrl;
--- get-search-url-next.js
+++ get-search-url-next.js
@@ -1,4 +1,4 @@
-function getSearchUrlNext(userId, before) {
+async function getSearchUrlNext(userId, before) {
   const params = new URLSearchParams({
     user_id: userId,
     before,
@@ -6,32 +6,33 @@ function getSearchUrlNext(userId, before) {
 
   const url = `https://www.dvdplaza.fi/search/member?${params.toString()}`;
 
-  return fetch(url, {
+  const res = await fetch(url, {
     method: "GET",
     redirect: "manual",
-  }).then((res) => {
-    const location = res.headers.get("location");
-    if (location) {
-      return location;
-    } else {
-      throw new Error("No Location header found in response");
-    }
   });
+
+  const location = res.headers.get("location");
+  if (location) {
+    return location;
+  } else {
+    throw new Error("No Location header found in response");
+  }
 }
 
 // If run directly, execute with parameters from CLI
 if (require.main === module) {
-  const userId = process.argv[2];
-  const before = process.argv[3];
-  getSearchUrlNext(userId, before)
-    .then((location) => {
+  (async () => {
+    const userId = process.argv[2];
+    const before = process.argv[3];
+    try {
+      const location = await getSearchUrlNext(userId, before);
       console.log(location);
       process.exit(0);
-    })
-    .catch((error) => {
+    } catch (error) {
       console.error("Error:", error.message);
       process.exit(1);
-    });
+    }
+  })();
 }
 
 module.exports = getSearchUrlNext;
